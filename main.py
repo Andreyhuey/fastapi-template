a4461469db830e00148ae0455b21f5f870ca5df7
Andreyhuey <andreyhuey777@gmail.com>
2024-07-02 08:30:02 +0100

    feat: another endpoint added

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "welcome to simlon medical center's backend"}
+
+@app.get("/wait")
+def wait():
+    return {"wait": "this works well now"}
